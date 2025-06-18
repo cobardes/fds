91a64b70e58e401d7e9f8d0e500c87133adb4304
rené <rene.morales.sanchez@gmail.com>
2025-06-18 19:41:43 -0400

    feat(schema): add new tables and columns

--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,13 +1,29 @@
 import { relations } from 'drizzle-orm/relations';
-import { venues, events } from './schema';
+import { venues, events, agendas, eventOccurrences } from './schema';
 
-export const eventsRelations = relations(events, ({ one }) => ({
+export const venuesRelations = relations(venues, ({ many }) => ({
+  agendas: many(agendas),
+  events: many(events),
+}));
+
+export const agendasRelations = relations(agendas, ({ one }) => ({
   venue: one(venues, {
-    fields: [events.venueId],
+    fields: [agendas.venueId],
     references: [venues.id],
   }),
 }));
 
-export const venuesRelations = relations(venues, ({ many }) => ({
-  events: many(events),
+export const eventsRelations = relations(events, ({ one, many }) => ({
+  venue: one(venues, {
+    fields: [events.venueId],
+    references: [venues.id],
+  }),
+  occurrences: many(eventOccurrences),
+}));
+
+export const eventOccurrencesRelations = relations(eventOccurrences, ({ one }) => ({
+  event: one(events, {
+    fields: [eventOccurrences.eventId],
+    references: [events.id],
+  }),
 }));
